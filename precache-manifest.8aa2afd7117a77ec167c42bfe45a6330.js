3c2f65b0854e33803e5ed83522d5c3a970baa068
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-10-10 23:04:10 -0300

    Updates

---
+++ precache-manifest.8aa2afd7117a77ec167c42bfe45a6330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "191d9301e6b290d418e1",
+    "url": "/static/js/main.191d9301.chunk.js"
+  },
+  {
+    "revision": "a3640a144bb8f7da7450",
+    "url": "/static/js/1.a3640a14.chunk.js"
+  },
+  {
+    "revision": "191d9301e6b290d418e1",
+    "url": "/static/css/main.79dcaa6f.chunk.css"
+  },
+  {
+    "revision": "6d3a64d661166b6bc07f4c4a7e6736e0",
+    "url": "/index.html"
+  }
+];
